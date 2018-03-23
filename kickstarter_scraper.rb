672c1e8adc970634e705830fcfb4982a6058a794
Christine Cua <cloudycua@gmail.com>
2018-03-23 18:56:24 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 
 require 'pry'
 
-# projects :kickstarter.css("li.project.grid_4")
+# projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
@@ -12,7 +12,14 @@ require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-    kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  #Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 
 
 end
